2393517a2c6cd5e8f66c520202ac64d535ade1e8
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 10:40:26 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+            tastaura*/
+            int i;
+            i = int.Parse(Console.ReadLine());
+            int lastdigit = i % 10;
+            Console.WriteLine(lastdigit);
+        }
+    }
+}
